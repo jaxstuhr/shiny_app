c95378f5a77cfade66efefc1c693863bc139bccd
jaxstuhr <jstuhr@ucsb.edu>
2022-03-14 16:07:12 -0700

    shiny app final-ish

--- Midden_Data_Viz/app.R
+++ Midden_Data_Viz/app.R
@@ -3,18 +3,53 @@ library(tidyverse)
 library(here)
 library(readxl)
 library(shinythemes)
+library(forcats)
+library(broom)
+library(janitor)
+library(sf)
+library(tmap)
 
 # Pull in data
 sample_mecs = read_xlsx(here("data", "sample_mecs_data.xlsx"))
+
+naics_labels = read_xlsx(here("data", "naics_codes.xlsx")) %>% 
+    select(naics_code = "2017 NAICS US   Code", name = "2017 NAICS US Title")
+
 long_mecs = sample_mecs %>% 
-    gather(fuel, value, 4:9)
-top_20 = sample_mecs %>% 
+    gather(fuel, value, 4:9)  %>% 
+    merge(naics_labels, by = "naics_code")
+    
+top_15 = sample_mecs %>% 
     filter(scenario == "Current Energy Consumption") %>% 
     filter(end_use == "TOTAL FUEL CONSUMPTION") %>% 
     mutate(total = net_electricity_demand + residual_fuel_oil + distillate_fuel_oil_and_diesel_fuel + 
                nat_gas + hgl_excluding_ng + coal_excluding_coke_and_breeze) %>% 
-    slice_max(order_by = total, n = 20) %>% 
-    gather(fuel, value, 4:10)
+    slice_max(order_by = total, n = 15) %>% 
+    gather(fuel, value, 4:10) %>% 
+    merge(naics_labels, by = "naics_code") %>% 
+    mutate(name = fct_reorder(name, value, .fun='sum'))
+
+
+us_regions_sf = read_sf(here("data", "us_regions", "jaxon_conus.shp"))
+
+regions_sf_sub = us_regions_sf %>%
+    clean_names() %>% 
+    select(region = name)
+
+regional_mecs = read_xlsx(here("data", "regional_mecs_data.xlsx"))
+
+regional_totals = regional_mecs %>% 
+    filter(end_use == "TOTAL FUEL CONSUMPTION") %>% 
+    mutate(net_electricity_demand = as.numeric(net_electricity_demand), 
+           residual_fuel_oil = as.numeric(residual_fuel_oil), 
+           diesel_fuel = as.numeric(diesel_fuel),
+           gas = as.numeric(gas),
+           natural_gas = as.numeric(natural_gas),
+           coke_and_breeze = as.numeric(coke_and_breeze)) 
+
+full_sf = merge(regions_sf_sub, regional_totals, by = "region") %>% 
+    gather(fuel, value, 3:8)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -30,27 +65,46 @@ ui <- fluidPage(
                           to the US manufacturing sector through adoption of best available energy-effficiency
                           measures. The primary data source is the Energy Information Administations 
                           (EIA) Manufacturing Energy Consumption Surveys (MECS). These surveys provide the energy 
-                          consumption by and use and fuel type for all manufacturing sectors (NAICS 311-339). Data from
-                          these surveys were compiled to characterize the current (2018) energy consumption scenario."),
+                          consumption by end-use and fuel type for all manufacturing sectors (NAICS 311-339). Data from
+                          these surveys were compiled to characterize the current (1518) energy consumption scenario."),
                         p("Currently, the Best Available Technology (BAT) adoption scenario is based on placeholder
                           coefficients for the consumpion reduction potential by end use and fuel type. Work is being
                           conducted to generate estimates for these coefficients be thermodynamic modelling of relevant 
-                          systems and a review of literature on energy efficiency options."),
-                        p("The figure to the right displays the total energy consumption for the 20 most energy 
-                          intensive manufacturing sectors. Subsequent tabs display this consupmtion sorted by end use 
-                          and fuel type for user-defined sectors and adoption rates. A characterization of energy-reduction
-                          potential by fuel type for defined end uses is provided in the final tab."),
+                          systems and a review of literature on energy efficiency improvement options."),
+                        p("The first figure (Tab 2) shows energy consumption by fuel type for the four US census regions. Subsequent tabs display 
+                          the current (1518) energy consumption by sector for the most intensive sectors, savings potential by end use and fuel type 
+                          with a user-defined BAT adoption rate, and a pite-chart of fuel_types with the greatest savings potential for a given end use."),
                         p("Data collection and analyses was performed by Jaxon Stuhr of the UCSB Bren School."),
                         strong("Data Citation:"),
                         p("cite MECS, energetics footprints, energy star, other literature here")
                     ), # END OF SIDEBAR PANEL
                     mainPanel(
-                        "Current Total Energy Usage of Top 20 Sectors",
-                        plotOutput("total_consumption")
+                        img(src = "industry_pic.jpg")
+                        #"Current Total Energy Usage of Top 15 Sectors",
+                        #plotOutput("total_consumption")
                     )
                 ) #end of sidebar layout
         ), # end of tabPanel 1
        ### End Tab 1
+       
+       ### Start Tab 2
+       tabPanel("Energy Consumption by Census Region and Fuel",
+                sidebarLayout(
+                    sidebarPanel(
+                        "Fuel Consumption",
+                        checkboxGroupInput(inputId = "region_fuel",
+                                           label = "Select Fuel Types: ",
+                                           choices = unique(full_sf$fuel),
+                                           selected = full_sf$fuel[1]
+                        )
+                    ), # END OF SIDEBAR PANEL
+                    mainPanel(
+                        strong("Energy Consumption by Census Region and Fuel"),
+                        plotOutput("region_plot")
+                    )
+                ) # END OF SIDEBAR LAYOU
+       ), # END OF TABPANEL "thing 1"),
+       ### End Tab 3
        
        ### Start Tab 2
        tabPanel("Current Energy Consumption by Sector",
@@ -59,16 +113,12 @@ ui <- fluidPage(
                         "Fuel Consumption",
                         checkboxGroupInput(inputId = "pick_fuel_types",
                                            label = "Select Fuel Types: ",
-                                           choices = unique(top_20$fuel)
-                        ), # END CHECKBOX GROUP INPUT
-                        "Top 20 Energy Consuming NAICS Sectors",
-                        radioButtons(inputId = "selected_naics_code_1",
-                                           label = "Select Sectors:",
-                                           choices = unique(top_20$naics_code)
-                        ) # END CHECKBOX GROUP INPUT
+                                           choices = unique(top_15$fuel),
+                                           selected = top_15$fuel[1]
+                        )
                     ), # END OF SIDEBAR PANEL
                     mainPanel(
-                        "Current Total Energy Usage of Top 20 Sectors",
+                        strong("Current Total Energy Consumption of Top 15 NAICS Sectors"),
                         plotOutput("consumption_plot")
                     )
                 ) # END OF SIDEBAR LAYOU
@@ -86,14 +136,14 @@ ui <- fluidPage(
                                     max = 100,
                                     value = 50
                                     ), # end slider input
-                        "Top 20 Energy Consuming NAICS Sectors",
+                        "Top 15 Energy Consuming NAICS Sectors",
                         radioButtons(inputId = "selected_naics_code_2",
                                      label = "Select Sectors:",
-                                     choices = unique(top_20$naics_code)
+                                     choices = unique(top_15$name)
                         )
                     ), # END OF SIDEBAR PANEL
                     mainPanel(
-                        "Energy Consumption Reduction Potentialby End Use",
+                        strong("Energy Consumption Reduction Potential by End Use"),
                         plotOutput("decarb_plot")
                         )
                     ) # END OF SIDEBAR LAYOU
@@ -111,14 +161,14 @@ ui <- fluidPage(
                                     max = 100,
                                     value = 50
                         ), # end slider input
-                        "Top 20 Energy Consuming NAICS Sectors",
+                        "Top 15 Energy Consuming NAICS Sectors",
                         radioButtons(inputId = "selected_naics_code_3",
                                      label = "Select Sectors:",
-                                     choices = unique(top_20$naics_code)
+                                     choices = unique(top_15$name)
                         )
                     ), # END OF SIDEBAR PANEL
                     mainPanel(
-                        "Energy Consumption Reduction Potential by Fuel Type",
+                        strong("Energy Consumption Reduction Potential by Fuel Type"),
                         plotOutput("fuel_plot")
                     )
                 ) # END OF SIDEBAR LAYOU
@@ -130,13 +180,13 @@ ui <- fluidPage(
                 sidebarLayout(
                     sidebarPanel(
                         "Sector Selector Radio Butons", 
-                        radioButtons(inputId = "select_end_use",
+                        selectInput(inputId = "select_end_use",
                                            label = "Choose End Use:",
                                            choices = unique(long_mecs$end_use)
                         ) # END CHECKBOX GROUP INPUT
                     ), # END OF SIDEBAR PANEL
                     mainPanel(
-                        "Reduction Potential Makeup by Fuel Types for Selected End Use", 
+                        strong("Reduction Potential Makeup by Fuel Types for Selected End Use"), 
                         plotOutput("pie_chart")
                     )
                 ) # END OF SIDEBAR LAYOUT), 
@@ -150,23 +200,36 @@ ui <- fluidPage(
 
 server = function(input, output) {
     
-    ### Start Tab 1 Reactive and Output ### 
-    output$total_consumption = renderPlot(
-        ggplot(data = top_20, aes(x = naics_code, y = value)) + 
-            geom_col(position = "dodge", fill = "darkgreen", color = "black") 
+    ### Start Tab 2 Reactive and Output ### 
+    region_reactive = reactive({
+        full_sf %>% 
+            filter(fuel %in% input$region_fuel)  %>% 
+            group_by(region) %>% 
+            summarise(value = sum(value))
+    }) # END "heat_map_reactive"
+    output$region_plot = renderPlot(
+        ggplot() + 
+            geom_sf(data = region_reactive(), aes(fill = value))  + 
+           # geom_sf_label(data = full_sf, aes(label = region)) +
+            theme_minimal() + 
+            labs(x = "", y = "") +
+            scale_fill_gradientn(colors = c("blue","purple"))
         #  geom_text(aes(label = value), color = "white", size=6) 
     )
-    ### End Tab 1 Reactive and Output ### 
+    ### End Tab 2 Reactive and Output ### 
     
     ### Start Tab 2 Reactive and Output ### 
     total_consumption_reactive = reactive({
-        top_20 %>%
-            filter(naics_code == input$selected_naics_code_1) %>%
-            filter(fuel %in% input$pick_fuel_types)
+        top_15 %>%
+            filter(fuel %in% input$pick_fuel_types)  %>% 
+            mutate(name = fct_reorder(name, desc(value), .fun = 'sum'))
     }) # END "heat_map_reactive"
     output$consumption_plot = renderPlot(
-        ggplot(data = total_consumption_reactive(), aes(x = fuel, y = value)) + 
-            geom_col(position = "dodge", fill = "darkgreen", color = "black") 
+        ggplot(data = total_consumption_reactive(), aes(x = name, y = value, fill = fuel)) + 
+            geom_col(position = "stack", color = "black") +
+            theme_minimal() +
+            coord_flip() + 
+            labs(x = "NAICS Sector", y = "Energy Consumption \n [TBtu]", fill = "Fuel Type")
         #  geom_text(aes(label = value), color = "white", size=6) 
     )
     ### End Tab 2 Reactive and Output ### 
@@ -174,28 +237,34 @@ server = function(input, output) {
     ### Start Tab 3 Reactive and Output ### 
     decarb_reactive = reactive({
         long_mecs %>%
-            filter(naics_code == input$selected_naics_code_2) %>% 
+            filter(name == input$selected_naics_code_2) %>% 
             filter(scenario %in% c("Current Energy Consumption", "Adoption of BATs")) %>% 
             group_by(end_use, scenario) %>% 
             summarise(value = sum(value) * input$adpt_rate_1)
     }) # END "decarb_reactive"
     output$decarb_plot = renderPlot(
         ggplot(data = decarb_reactive(), aes(x = end_use, y = value, fill = scenario)) +
-            geom_col(position = "dodge")
+            geom_col(position = "dodge", color = "black") + 
+            theme_minimal() +
+            coord_flip() +
+            labs(x = "End Use", y = "Energy Consumption \n [TBtu]", fill = "Scenario")
     ) # END "output$sw_plot
     ### End Tab 3 Reactive and Output ### 
     
     ### Start Tab 4 Reactive and Output ### 
     fuel_type_reactive = reactive({
         long_mecs %>%
-            filter(naics_code == input$selected_naics_code_3) %>% 
+            filter(name == input$selected_naics_code_3) %>% 
             filter(scenario %in% c("Current Energy Consumption", "Adoption of BATs")) %>% 
             group_by(fuel, scenario) %>% 
             summarise(value = sum(value) * input$adpt_rate_2)
     }) # END "decarb_reactive"
     output$fuel_plot = renderPlot(
         ggplot(data = fuel_type_reactive(), aes(x = fuel, y = value, fill = scenario)) +
-            geom_col(position = "dodge")
+            geom_col(position = "dodge", color = "black") + 
+            theme_minimal() +
+            coord_flip() +
+            labs(x = "Fuel Type", y = "Energy Consumption \n [TBtu]", fill = "Scenario")
     ) # END "output$sw_plot
     ### End Tab 4 Reactive and Output ### 
     
@@ -209,9 +278,10 @@ server = function(input, output) {
     }) # END "heat_map_reactive"
     output$pie_chart = renderPlot(
         ggplot(data = pie_chart_reactive(), aes(x = "", y = value, fill = fuel)) +
-            geom_bar(stat="identity", width=1, color="white") +
+            geom_bar(stat="identity", width=1, color="black") +
             coord_polar("y", start=0) + 
-            theme_void() 
+            theme_minimal() +
+            labs(x = "", y = "Energy Consumption \n [TBtu]",  fill = "Fuel Type")
           #  geom_text(aes(label = value), color = "white", size=6) 
     )
     ### End Tab 5 Reactive and Output ### 
